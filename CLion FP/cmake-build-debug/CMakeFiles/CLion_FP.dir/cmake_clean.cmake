5b6933c90d9881b12c90b310338bf2fd8e8f3bf2
Artemiiik <ar.yunin@mail.ru>
2021-08-10 01:49:21 +0300

    Clion Git Test

---
+++ CLion FP/cmake-build-debug/CMakeFiles/CLion_FP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CLion_FP.exe"
+  "CLion_FP.exe.manifest"
+  "CLion_FP.pdb"
+  "CMakeFiles/CLion_FP.dir/main.cpp.obj"
+  "libCLion_FP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CLion_FP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CLion FP/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <Windows.h>
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    std::cout << "Привет! Это мой первый проект в CLion)" << std::endl;
+    return 0;
+}
